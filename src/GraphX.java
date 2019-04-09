becd8656772f6a92ab70da551d4e3bf78325d2c9
Ilham <i.belkhadir09@gmail.com>
2019-04-09 10:33:31 -0400

    Scala project

--- src/GraphX.java
+++ src/GraphX.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
+import org.apache.spark.{SparkConf}
+import org.apache.spark.SparkContext
+import org.apache.spark.graphx.{Edge, EdgeContext, Graph, _}
 
 public class GraphX {
 }
